0b0094d272b85ae819fd635c2cf216026a971553
Dylan <me@delxhq.me>
2022-02-06 17:14:58 +0000

    We don't need to start a Java server

--- src/main/java/me/delxhq/bridge/Bridge.java
+++ src/main/java/me/delxhq/bridge/Bridge.java
@@ -5,19 +5,13 @@ import net.fabricmc.api.ModInitializer;
 public class Bridge implements ModInitializer {
 
 	private Client client;
-	private Server server;
 
 	@Override
 	public void onInitialize() {
 		this.client = new Client(this);
-		this.server = new Server(this);
 	}
 
 	public Client getClient() {
 		return client;
-	}
-
-	public Server getServer() {
-		return server;
 	}
 }
